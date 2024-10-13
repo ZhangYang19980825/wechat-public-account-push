77600f3240ced059ae91905fe103d43f2171ddb4
ZhangYang19980825 <163385540+ZhangYang19980825@users.noreply.github.com>
2024-10-13 23:27:56 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -2,7 +2,7 @@
 const TEMPLATE_CONFIG = [
   {
     id: '0001',
-    title: '亲爱的, 早上好',
+    title: '宝宝, 早上好',
     desc: `
       **{{date.DATA}}**
       
@@ -34,24 +34,7 @@ const TEMPLATE_CONFIG = [
       
       {{poetry_title.DATA}} {{poetry_content.DATA}}
     `
-  },
-  {
-    id: '0002',
-    title: '推送完成提醒',
-    desc: `
-      服务器信息：{{post_time_zone.DATA}} {{post_time.DATA}}
-      
-      ---
-      
-      共推送 {{need_post_num.DATA}} 人
-      
-      成功: {{success_post_num.DATA}} | 失败: {{fail_post_num.DATA}}
-      
-      成功用户: {{success_post_ids.DATA}}
-      
-      失败用户: {{fail_post_ids.DATA}}
-    `
-  },
+  }
 ]
 
 module.exports = TEMPLATE_CONFIG
